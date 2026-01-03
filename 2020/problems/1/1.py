89404774c236c17125226b6aa428e71dd85c9357
duckyduck321 <althariacc@gmail.com>
2026-01-02 20:49:16 -0800

    new code for colorland hari

--- 2020/problems/1/1.py
+++ 2020/problems/1/1.py
@@ -1,20 +1,22 @@
-n = int(input())
-possible = ["Blue", "Orange", "Pink", "Green", "Red", "Yellow"]
-seen = []
-best = []
-count = 0
-for _ in range(n):
-    inp = input()
-    if inp not in seen:
-        seen.append(inp)
-    else:
-        count += 1
-    if len(seen) == len(possible):
-        seen = []
-        best.append(inp)
-        count = 0
-print(len(best) + count + (1 if seen else 0))
+# n = int(input())
+# possible = ["Blue", "Orange", "Pink", "Green", "Red", "Yellow"]
+# seen = []
+# best = []
+# count = 0
+# for _ in range(n):
+#     inp = input()
+#     if inp not in seen:
+#         seen.append(inp)
+#     else:
+#         count += 1
+#     if len(seen) == len(possible):
+#         seen = []
+#         best.append(inp)
+#         count = 0
+# print(len(best) + count + (1 if seen else 0))
+
 # hari code
+from itertools import zip_longest
 n = int(input())
 arr = []
 for i in range(n):
@@ -25,26 +27,32 @@ else:
         pos = -1
         # Try every color each time and whatever gets u the furthest 
         # stick with that don't need to store the color just a while loop
+        # Instead of checking every color every time, we can check 
+        # for all indices of all colors, and get the closest index after pos + 1
         cnt = 0
-        while pos != n - 1:
-                blue = 0
-                orange = 0
-                pink = 0
-                green = 0
-                red = 0
-                yellow = 0
-                if 'Blue' in arr[pos + 1:]:
-                        blue = arr.index('Blue', pos + 1)
-                if 'Orange' in arr[pos + 1:]:
-                        orange = arr.index('Orange', pos + 1)
-                if 'Pink' in arr[pos + 1:]:
-                        pink = arr.index('Pink', pos + 1)
-                if 'Green' in arr[pos + 1:]:
-                        green = arr.index('Green', pos + 1)
-                if 'Red' in arr[pos: + 1]:
-                        red = arr.index('Red', pos + 1)
-                if 'Yellow' in arr[pos + 1:]:
-                        yellow = arr.index('Yellow', pos + 1)
-                pos = max(blue, orange, pink, green, red, yellow)
+        blue = []
+        orange = []
+        pink = []
+        green = []
+        red = []
+        yellow = []
+        for i in range(n):
+                if arr[i] == 'Blue':
+                        blue.append(i)
+                if arr[i] == 'Orange':
+                        orange.append(i)
+                if arr[i] == 'Pink':
+                        pink.append(i)
+                if arr[i] == 'Green':
+                        green.append(i)
+                if arr[i] == 'Red':
+                        red.append(i)
+                if arr[i] == 'Yellow':
+                        yellow.append(i)
+        big = list(zip_longest(blue, orange, pink, green, red, yellow, fillvalue = -1))
+        cnt = 0
+        g = 0
+        while g < n - 1:
+                g = max(big[cnt])
                 cnt += 1
 print(cnt)
